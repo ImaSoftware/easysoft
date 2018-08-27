2db3d0f07ed35bac443baba34920b05699eea275
daversoft <daversoft@outlook.com>
2018-08-27 17:19:36 -0500

    FrmRep10001

---
+++ easySoft/GUI/Interfaz/Reportes/FrmRep10001.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.Interfaz.Reportes
+{
+    public partial class FrmRep10001 : GUI.baseClass.Repform
+    {
+        public FrmRep10001()
+        {
+            InitializeComponent();
+            this.nameReport = "R_10001";
+        }
+        public FrmRep10001(string xtitulo, string args)
+        {
+            InitializeComponent();
+            this.nameReport = "R_10001";
+        }
+        public override void RecogeParametros()
+        {
+            this.Paramis.Clear();
+           // Paramis.Add(new DLIB.Parametro("MES", fecIni.Value.Month));
+           // Paramis.Add(new DLIB.Parametro("ANIO", fecIni.Value.Year));
+        }
+    }
+  
+}
